6dceae44b25be56308ec4f124a92d0a92f0e78aa
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-01-13 15:06:47 -0300

    Entendendo sobre Urls, Views e Templates.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from tasks.views import helloWorld
+from tasks.views import helloWorld, taskList, yourName
 
 urlpatterns = [
     path('helloWorld/', helloWorld),
+    path('', taskList, name='task-list'),
+    path('yourname/<str:name>', yourName, name='your-name')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 def helloWorld(request):
     return HttpResponse('Hello World')
 
+def taskList(request):
+    return render(request, 'tasks/list.html')
+
+def yourName(request, name):
+    return  render(request, 'tasks/yourname.html', {'name': name})
