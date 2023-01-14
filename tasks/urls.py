f4b79e875f645963fd35f5c29614a5bc28dd8827
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-01-14 12:19:24 -0300

    Editando dados do banco.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from tasks.views import helloWorld, taskList, taskView, yourName, newTask
+from tasks.views import helloWorld, taskList, taskView, yourName, newTask, editTask
 
 urlpatterns = [
     path('helloWorld/', helloWorld),
     path('', taskList, name='task-list'),
     path('task/<int:id>', taskView, name='task-list'),
     path('newtask/', newTask, name='new-task'),
+    path('edit/<int:id>', editTask, name='edit'), #O Id é utilizado para mostar os dados atuais para saber o que vai editar.
     path('yourname/<str:name>', yourName, name='your-name')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -8,7 +8,6 @@ def taskList(request):
     tasks = Task.objects.all().order_by('-created_at')
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
-
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
@@ -27,6 +26,21 @@ def newTask(request):
 
 
     return render(request, 'tasks/addtask.html', {'form': form})
+
+
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task) #Deixa o formulario com os dados anteriores
+    
+    if (request.method == 'POST'):
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task.save()
+            return redirect('/')
+        else:
+            return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+    else:
+        return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
 
 def helloWorld(request):
     return HttpResponse('Hello World')
