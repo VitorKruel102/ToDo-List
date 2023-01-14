cd71672eaee55e1eae1f48b8d2d1a196bac846f2
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-01-14 19:00:58 -0300

    Melhorando DashBoard. Projeto FInalizado.

--- tasks/views.py
+++ tasks/views.py
@@ -3,28 +3,34 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.contrib import messages
-
+import datetime
 
 from .models import Task
 from .forms import TaskForm
 
 @login_required
 def taskList(request):
+    
     search = request.GET.get('search')
     filter = request.GET.get('filter')
+    tasksDoneRecently = Task.objects.filter(done='done', updated_at__gt=datetime.datetime.now()-datetime.timedelta(days=30)).count()
+    tasksDone = Task.objects.filter(done='done', user=request.user).count()
+    tasksDoing = Task.objects.filter(done='doing', user=request.user).count()
 
     if search:
-        tasks = Task.objects.filter(title__icontains=search, user=request.user) #title_icontains -> Para o usuario não precisar saber exatamente o titulo
+        tasks = Task.objects.filter(title__icontains=search, user=request.user)
     elif filter:
         tasks = Task.objects.filter(done=filter, user=request.user)
     else:
         tasks_list = Task.objects.all().order_by('-created_at').filter(user=request.user)
 
         paginator = Paginator(tasks_list, 3)
+
         page = request.GET.get('page')
         tasks = paginator.get_page(page)
 
-    return render(request, 'tasks/list.html', {'tasks': tasks})
+    return render(request, 'tasks/list.html', 
+        {'tasks':tasks, 'tasksrecently': tasksDoneRecently, 'tasksdone': tasksDone, 'tasksdoing': tasksDoing })
 
 
 @login_required
