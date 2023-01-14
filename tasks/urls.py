badb007cff7b229fd6290ba75d06c46c8c3efb6c
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-01-14 09:40:26 -0300

    Criado a conexão entre o Banco de Dados e o template.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from tasks.views import helloWorld, taskList, yourName
+from tasks.views import helloWorld, taskList, taskView, yourName
 
 urlpatterns = [
     path('helloWorld/', helloWorld),
     path('', taskList, name='task-list'),
+    path('task/<int:id>', taskView, name='task-list'),
     path('yourname/<str:name>', yourName, name='your-name')
 ]
