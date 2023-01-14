ff37b3d702a352e90e088e538c17bb1a40f03a2f
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-01-14 11:56:15 -0300

    Adicionando dados no banco.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from tasks.views import helloWorld, taskList, taskView, yourName
+from tasks.views import helloWorld, taskList, taskView, yourName, newTask
 
 urlpatterns = [
     path('helloWorld/', helloWorld),
     path('', taskList, name='task-list'),
     path('task/<int:id>', taskView, name='task-list'),
+    path('newtask/', newTask, name='new-task'),
     path('yourname/<str:name>', yourName, name='your-name')
 ]
