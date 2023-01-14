a74780ba78f4d51f9cfe665fc1c9eaf36e8c8a0e
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-01-14 18:29:23 -0300

    Criando filtros das tarefas.

--- tasks/views.py
+++ tasks/views.py
@@ -11,8 +11,12 @@ from .forms import TaskForm
 @login_required
 def taskList(request):
     search = request.GET.get('search')
+    filter = request.GET.get('filter')
+
     if search:
         tasks = Task.objects.filter(title__icontains=search, user=request.user) #title_icontains -> Para o usuario não precisar saber exatamente o titulo
+    elif filter:
+        tasks = Task.objects.filter(done=filter, user=request.user)
     else:
         tasks_list = Task.objects.all().order_by('-created_at').filter(user=request.user)
 
