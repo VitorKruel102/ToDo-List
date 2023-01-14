a72b893382597904c26cff2951cba1b0fff2e753
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-01-14 13:19:36 -0300

    Trabalhando paginação.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from tasks.views import helloWorld, taskList, taskView, yourName, newTask, editTask
+from tasks.views import helloWorld, taskList, taskView, yourName, newTask, editTask, deleteTask
 
 urlpatterns = [
     path('helloWorld/', helloWorld),
     path('', taskList, name='task-list'),
     path('task/<int:id>', taskView, name='task-list'),
     path('newtask/', newTask, name='new-task'),
-    path('edit/<int:id>', editTask, name='edit'), #O Id é utilizado para mostar os dados atuais para saber o que vai editar.
+    path('edit/<int:id>', editTask, name='edit-task'), #O Id é utilizado para mostar os dados atuais para saber o que vai editar.
+    path('delete/<int:id>', deleteTask, name='delete-task'),
     path('yourname/<str:name>', yourName, name='your-name')
 ]
